2d0da1568c1d899d6e75c4e49636eb689cd8260f
Otavio R. Piske <angusyoung@gmail.com>
2018-04-07 09:01:10 +0200

    UPD: remove unused variable

--- src/main/java/net/orpiske/mdp/utils/Constants.java
+++ src/main/java/net/orpiske/mdp/utils/Constants.java
@@ -20,9 +20,6 @@ package net.orpiske.mdp.utils;
  *
  */
 public final class Constants {
-
-	public static final String VERSION = "1.0.0";
-
 	public static final String BIN_NAME = "mpt-data-plotter";
 
 
